8a8912162fc9228722d1cdee74ceb9cbc682dfbb
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-03-04 09:55:44 +0100

    The method signature should be the same.
    The Choices class extends BaseClass but has another method signature
    for __construct(). Since the existence of a constructor is implied in a
    class this abstract mention is not required nor useful.
    
    ps. PHP 5.4 craps out over this change in signature

--- tropo.class.php
+++ tropo.class.php
@@ -634,12 +634,6 @@ class Tropo extends BaseClass {
 abstract class BaseClass {
 
 	/**
-	 * Class constructor
-	 * @abstract __construct()
-	 */
-	abstract public function __construct();
-
-	/**
 	 * toString Function
 	 * @abstract __toString()
 	 */
