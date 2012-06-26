aa4c88f786aeae78c41441f6feae78816e004b8a
JustinDupree <jdupree@voxeo.com>
2012-06-26 10:15:54 -0300

    Updated to omit portions of code that caused confusion and looping applications

--- samples/outbound_call.php
+++ samples/outbound_call.php
@@ -1,58 +1,28 @@
-<?php
-// To make an outgoing call, simply add your
-// Tropo application token and the number you
-// want to dial to the settings below. Then 
-// put this on your web server and load it
-// in your browser.
+<?php 
 
-// Settings! Shiny.
-$token = 'your token here';
-$number = 'the number you would like to dial';
+//this example accepts parameters passed to it from an outside REST request (such as a curl app) - 
+//it extracts the values of the passed parameters and uses it to send, in this case, an outbound text
+ 
+require 'tropo.class.php';
 
-// Include the Tropo library and create a Tropo object
-include_once 'tropo.class.php';
-$tropo = new Tropo();
+//brings in the Tropo library
+ 
+$session = new Session(); 
+$to = "+".$session->getParameters("numbertodial"); 
+$name = $session->getParameters("customername"); 
+$msg = $session->getParameters("msg"); 
 
-// When the the session object is created, it tries
-// to load the json that Tropo posts when reciving or
-// making a call. If the json doesn't exist, the 
-// Session object throws a TropoException.
-// This try/catch block checks to see if this code is
-// being run as part of a session or being run directly.
-try {
-  
-  // this next line throws an exception if the code isn't
-  // being run by Tropo. If that happens, the catch block
-  // below will run.
-  $session = new Session();
-  
-  if ($session->getParameters("action") == "create") {  
-    // A token-launched session (an outgoing call) will
-    // have a parameter called "action" that is set to
-    // "create". If this is true, we're trying to make an
-    // outgoing call. The next two lines make that call
-    // and say something.
-    $tropo->call($session->getParameters("dial"));
-    $tropo->say('This is an outbound call.');
-  } else {
-    
-    // The session JSON exists, but there's no action 
-    // parameter or it wasn't set to "create" so this must 
-    // be an incoming call.
-    $tropo->say('Thank you for calling us.');
-  }
-  $tropo->renderJSON();
-} catch (TropoException $e) {
-  if ($e->getCode() == '1') {
-    
-    // The session object threw an exception, so this file wasn't
-    // loaded as part of a Tropo session. Use the session API to 
-    // launch a new session.
-    if ($tropo->createSession($token, array('dial' => $number))) {
-      print 'Call launched to ' . $number;
-    } else {
-      print "call failed! Try it again with the Tropo debugger running to see what the error is.";
-    }
-  }
-}
-?>
+//extracts the contects of the passed parameters and assigns them as variables for later use
+
+$tropo = new Tropo(); 
+     
+$tropo->call($to, array('network'=>'SMS')); 
+$tropo->say("OMG ".$name.", ".$msg."!"); 
+
+//actually creates the call, passed the "to" value and then adds in the other variables for the message
+
+return $tropo->RenderJson(); 
+
+//defines the response to Tropo in JSON
+ 
+?> 
