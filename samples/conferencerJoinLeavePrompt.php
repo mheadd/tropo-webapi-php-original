392ef6997a9e552c8edf1507e27e8bd1ebdd315a
Kevin Bond <kbond@voxeolabs.com>
2013-11-25 12:18:50 -0500

    Added machine detection to the transfer method

--- samples/conferencerJoinLeavePrompt.php
+++ samples/conferencerJoinLeavePrompt.php
@@ -14,6 +14,7 @@ $tropo->say("You are about to enter the conference");
 
 $tropo->conference(null, array(
   "id"=>"1234",
+  "name"=>"joinleave",
   "joinPrompt" => "$callerID has entered the conference",
   "leavePrompt" => "$callerID has left the conference", 
   "voice" => "Victor",
